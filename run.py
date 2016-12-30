93b94c075d318ac9d1065b46a883ce4b50572c70
iam4570 <iam4570@yahoo.co.jp>
2016-12-30 17:14:04 +0900

    fix port2

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os
 from flask import Flask
 app = Flask(__file__)
 
 @app.route('/')
 def hello():
     return 'Hello World'
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run(port=port)
